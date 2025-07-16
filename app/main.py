12d9aad7df1130530c6340138a384ca95801f316
Ilnar Valiullin <awesome.ikn@yandex.ru>
2025-07-16 12:52:45 +0300

    edit Readme and add endpoint /connect

--- app/main.py
+++ app/main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
+from paramiko import SSHClient
+
+client = SSHClient()
+client.load_system_host_keys()
+client.connect("45.137.188.181", username="base_user", port=22)
+
+stdin, stdout, stderr = client.exec_command('ls -al')
+data = stdout.read() + stderr.read()
+
+client.close()
 
 app = FastAPI()
 
 @app.get("/")
 async def hello():
-    return {"Hello" : "world!"}
+    return {"Hello" : data}
+
+@app.get("/connect")
+async def connect(host: str, username: str):
+    client.connect(hostname=host, username=username, port=22)
+    stdin, stdout, stderr = client.exec_command("whoami")
+
+    data = stdout.read() + stderr.read()
+    return {"response" : data}
 
 if __name__ == "__main__":
     app.run()
